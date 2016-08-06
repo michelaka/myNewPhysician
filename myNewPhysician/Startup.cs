6a11483076428dde654f05b888f9d5d07fb33065
eljed1 <eljed1@DESKTOP-A00OVL9>
2016-08-06 01:35:33 -0700

    Add project files.

---
+++ myNewPhysician/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(myNewPhysician.Startup))]
+namespace myNewPhysician
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
